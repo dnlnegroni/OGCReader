a0b4568d94f7a81ef232fb8f6d69e9568d7eeaf3
lorenzoviva <lorenzo.vaccaro@hotmail.com>
2015-06-11 18:29:21 +0200

    asd

--- qRReaderExample/src/main/java/com/example/qr_readerexample/OgcApp.java
+++ qRReaderExample/src/main/java/com/example/qr_readerexample/OgcApp.java
@@ -2,7 +2,9 @@ package com.example.qr_readerexample;
 
 import android.app.Application;
 
+import com.example.qr_readerexample.com.example.qr_readerexample.model.QREntity;
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 /**
  * Created by Emanuele on 11/06/2015.
@@ -12,6 +14,7 @@ public class OgcApp extends Application {
     @Override
     public void onCreate() {
         Parse.enableLocalDatastore(this);
+        ParseObject.registerSubclass(QREntity.class);
         Parse.initialize(this, "60uOLFg0uCx3rwfuvySqnejIR478Y8IjQDb6jf4h", "xVrs6cXx5gpgtjvFRf0f4Eh2poWHLEAEpvb24mq1");
     }
 }
