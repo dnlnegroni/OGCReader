acdddb410e447043b032effdf54eb90e095e0b9e
daniele <dnl.negroni@gmail.com>
2015-06-11 17:26:43 +0200

    core: flash

---
+++ qRReaderExample/src/main/java/com/example/qr_readerexample/com/example/qr_readerexample/interseption/DrawingView.java
@@ -0,0 +1,21 @@
+package com.example.qr_readerexample.com.example.qr_readerexample.interseption;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * Created by daniele on 11/06/15.
+ */
+public class DrawingView {
+
+    public DrawingView(Context context, AttributeSet attrs){
+        //super(context, attrs);
+        setupDrawing();
+    }
+
+    private void setupDrawing(){
+        //get drawing area setup for interaction
+    }
+
+
+}
