788a9c4332796c68ffd25a7d8a203a5b67bb88d8
lorenzoviva <lorenzo.vaccaro@hotmail.com>
2015-06-11 18:21:39 +0200

    -aggiunto metodo basato su PerpectiveTransform di zxing per mettere i prospettiva l'immagine -scarsi risultati : velocità media di elaborazione 3000ms, inoltre le immagini deformate sono sempre spostate (forse ho scambiato x e y?)

---
+++ qRReaderExample/src/main/java/com/example/qr_readerexample/com/example/qr_readerexample/model/QREntity.java
@@ -0,0 +1,45 @@
+package com.example.qr_readerexample.com.example.qr_readerexample.model;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import com.parse.GetDataCallback;
+import com.parse.ParseClassName;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+
+import java.io.ByteArrayOutputStream;
+
+
+/**
+ * Created by root on 11/06/15.
+ */
+@ParseClassName("QREntity")
+public class QREntity extends ParseObject {
+
+        public QREntity(){
+
+        }
+
+        public void setQRURL(String qrurl){
+            this.put("QRURL",qrurl);
+        }
+        public String getQRURL(){
+            return this.getString("QRURL");
+        }
+        public void setRepresentation(Bitmap bitmap){
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            byte[] image = stream.toByteArray();
+            ParseFile file = new ParseFile("android.png", image);
+            this.put("ImageFile", file);
+            this.saveInBackground();
+        }
+        public  Bitmap getRepresentation() throws com.parse.ParseException{
+            ParseFile file =  this.getParseFile("ImageFile");
+            byte[] image = file.getData();
+            Bitmap bitmap = BitmapFactory .decodeByteArray(image, 0, image.length);
+            return bitmap;
+        }
+}
